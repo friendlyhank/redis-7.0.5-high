f032e2f8b481239af4fcac3fded052c5dcf6b5b6
hank <hanguang8888@outlook.com>
2022-09-04 22:48:49 +0800

    server.sh

---
+++ src/server.h
@@ -0,0 +1,18 @@
+
+/* Log levels 日志等级 */
+#define LL_DEBUG 0
+#define LL_NOTICE 2
+#define LL_WARNING 3
+
+int main(int argc, char **argv) {
+    serverLog(LL_WARNING, "oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo");
+}
+
+/* Use macro for checking log level to avoid evaluating arguments in cases log
+ * should be ignored due to low level. */
+// 服务端日志打印
+#define serverLog(level, ...) do {\
+        if (((level)&0xff) < server.verbosity) break;\
+        _serverLog(level, __VA_ARGS__);\
+    } while(0)
+
