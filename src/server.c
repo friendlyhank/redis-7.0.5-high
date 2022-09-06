eab8ffffc463b7a9dd488db3fbe0a37c20148a76
李汉光 <lihanguang@xinyu668.com>
2022-09-06 09:02:12 +0800

    makefile

--- src/server.c
+++ src/server.c
@@ -5,14 +5,14 @@
 #define LL_WARNING 3
 
 int main(int argc, char **argv) {
-    serverLog(LL_WARNING, "oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo");
+//    serverLog(LL_WARNING, "oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo");
 }
 
 /* Use macro for checking log level to avoid evaluating arguments in cases log
  * should be ignored due to low level. */
 // 服务端日志打印
-#define serverLog(level, ...) do {\
-        if (((level)&0xff) < server.verbosity) break;\
-        _serverLog(level, __VA_ARGS__);\
-    } while(0)
+//#define serverLog(level, ...) do {\
+//        if (((level)&0xff) < server.verbosity) break;\
+//        _serverLog(level, __VA_ARGS__);\
+//    } while(0)
 
