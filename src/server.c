48e87df23b9757f2cbe5a6a01bcfdb8e7f6df46c
李汉光 <lihanguang@xinyu668.com>
2022-09-09 09:24:20 +0800

    server log

--- src/server.c
+++ src/server.c
@@ -66,39 +66,39 @@ void serverLogRaw(int level, const char *msg) {
     FILE *fp;//文件描述符
     char buf[64];
     int rawmode = (level & LL_RAW); // 是否需要打印时间
-    int log_to_stdout = server.logfile[0] == '\0';//控制台输出或文件输出
-
-    level &= 0xff; /*判断等级参数 clear flags */
-    if (level < server.verbosity) return;
-
-    fp = log_to_stdout ? stdout : fopen(server.logfile,"a");
-    if (!fp) return;
-
-    if (rawmode) {
-        fprintf(fp,"%s",msg);
-    }else{
-        int off;
-        struct timeval tv; // 时间
-        int role_char; // 日志标识
-        pid_t pid = getpid();// 获取进程pid
-
-        gettimeofday(&tv,NULL);
-        struct tm tm;
-        off = strftime(buf,sizeof(buf),"%d %b %Y %H:%M:%S.",&tm);
-        snprintf(buf+off,sizeof(buf)-off,"%03d",(int)tv.tv_usec/1000);
-        if (server.sentinel_mode) {
-            role_char = 'X'; /* Sentinel. */
-        }else if (pid != server.pid) {//是否主进程
-            role_char = 'C'; /*不是主进程则是RDB,AOF的子进程在跑 RDB / AOF writing child. */
-        }else{
-            role_char = (server.masterhost ? 'S':'M'); /*主节点用M,子节点用S Slave or Master. */
-        }
-        fprintf(fp,"%d:%c %s %c %s\n",
-                (int)getpid(),role_char, buf,c[level],msg);
-    }
-    fflush(fp);
-    if (!log_to_stdout) fclose(fp);//如果不是控制台输出则关闭文件
-    if (server.syslog_enabled) syslog(syslogLevelMap[level], "%s", msg);// 系统日志输出
+//    int log_to_stdout = server.logfile[0] == '\0';//控制台输出或文件输出
+//
+//    level &= 0xff; /*判断等级参数 clear flags */
+//    if (level < server.verbosity) return;
+//
+//    fp = log_to_stdout ? stdout : fopen(server.logfile,"a");
+//    if (!fp) return;
+//
+//    if (rawmode) {
+//        fprintf(fp,"%s",msg);
+//    }else{
+//        int off;
+//        struct timeval tv; // 时间
+//        int role_char; // 日志标识
+//        pid_t pid = getpid();// 获取进程pid
+//
+//        gettimeofday(&tv,NULL);
+//        struct tm tm;
+//        off = strftime(buf,sizeof(buf),"%d %b %Y %H:%M:%S.",&tm);
+//        snprintf(buf+off,sizeof(buf)-off,"%03d",(int)tv.tv_usec/1000);
+//        if (server.sentinel_mode) {
+//            role_char = 'X'; /* Sentinel. */
+//        }else if (pid != server.pid) {//是否主进程
+//            role_char = 'C'; /*不是主进程则是RDB,AOF的子进程在跑 RDB / AOF writing child. */
+//        }else{
+//            role_char = (server.masterhost ? 'S':'M'); /*主节点用M,子节点用S Slave or Master. */
+//        }
+//        fprintf(fp,"%d:%c %s %c %s\n",
+//                (int)getpid(),role_char, buf,c[level],msg);
+//    }
+//    fflush(fp);
+//    if (!log_to_stdout) fclose(fp);//如果不是控制台输出则关闭文件
+//    if (server.syslog_enabled) syslog(syslogLevelMap[level], "%s", msg);// 系统日志输出
 }
 
 /* Like serverLogRaw() but with printf-alike support. This is the function that
