cb206fed11a84d2cfb7b77463d1a7cb040793024
李汉光 <lihanguang@xinyu668.com>
2022-09-16 09:06:29 +0800

    config

--- src/config.c
+++ src/config.c
@@ -1,4 +1,17 @@
 
+// 标准配置信息
+typedef struct standardConfig standardConfig;
+
+// 标准配置信息结构体
+struct standardConfig {
+
+};
+
+// 配置信息结构体
+standardConfig static_configs[] = {
+
+};
+
 /* Initialize configs to their default values and create and populate the
  * runtime configuration dictionary.
  * 初始化默认配置信息并且创建配置字段
